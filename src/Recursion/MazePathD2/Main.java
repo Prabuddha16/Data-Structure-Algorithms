75debf0e663bde4680e3399d1f6e85d31671cd9a
Prabuddha16 <prabuddhakumar97@gmail.com>
2022-05-10 15:19:42 +0530

    update

--- src/Recursion/MazePathD2/Main.java
+++ src/Recursion/MazePathD2/Main.java
@@ -2,7 +2,7 @@ package Recursion.MazePathD2;
 
 import java.util.*;
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int a = path(1, 1, n, n, "");
