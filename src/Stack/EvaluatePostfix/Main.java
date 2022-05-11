418c81b1f4476d70c988495a7919199b9407737f
Prabuddha16 <prabuddhakumar97@gmail.com>
2022-05-11 08:00:46 +0530

    update

---
+++ src/Stack/EvaluatePostfix/Main.java
@@ -0,0 +1,57 @@
+package Stack.EvaluatePostfix;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+class Main
+{
+    static int evaluatePostfix(String exp)
+    {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < exp.length(); i++)
+        {
+            char c = exp.charAt(i);
+
+            if(c == ' ') {
+                continue;
+            }
+
+            else if(Character.isDigit(c))
+            {
+                int n = 0;
+                while(Character.isDigit(c))
+                {
+                    n = n*10 + (c-'0');
+                    i++;
+                    c = exp.charAt(i);
+                }
+                i--;
+
+                stack.push(n);
+            }
+
+            else
+            {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+
+                switch (c) {
+                    case '+' -> stack.push(val2 + val1);
+                    case '-' -> stack.push(val2 - val1);
+                    case '/' -> stack.push(val2 / val1);
+                    case '*' -> stack.push(val2 * val1);
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String exp = sc.nextLine();
+        System.out.println(evaluatePostfix(exp));
+    }
+}
+
---
+++ src/Stack/ReverseString/Main.java
@@ -0,0 +1,25 @@
+package Stack.ReverseString;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static String reverse(String S){
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<S.length();i++){
+            st.push(S.charAt(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String ans = reverse(s);
+        System.out.println(ans);
+    }
+}
