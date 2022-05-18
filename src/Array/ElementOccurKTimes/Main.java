93b2e13d9ed58e38f75c02fa9ffecaf5002061e1
Prabuddha16 <prabuddhakumar97@gmail.com>
2022-05-18 18:16:23 +0530

    update

---
+++ src/Array/ElementOccurKTimes/Main.java
@@ -0,0 +1,21 @@
+package Array.ElementOccurKTimes;
+
+import java.util.HashMap;
+
+class Main
+{
+    public int firstElementKTime(int[] arr, int n, int k) {
+        HashMap<Integer, Integer> s = new HashMap<Integer, Integer>();
+        for(int i=0; i<n; i++){
+            if(s.containsKey(arr[i])) {
+                s.put(arr[i], s.get(arr[i]) + 1);
+            } else {
+                s.put(arr[i], 1);
+            }
+            if(s.get(arr[i])==k){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/Array/PositiveNegativeArrange/Main.java
@@ -0,0 +1,4 @@
+package Array.PositiveNegativeArrange;
+
+public class Main {
+}
