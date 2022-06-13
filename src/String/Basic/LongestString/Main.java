cc5becbf16fe0229810f638f3211c0f56edcc47f
Prabuddha16 <prabuddhakumar97@gmail.com>
2022-06-13 21:32:27 +0530

    update

---
+++ src/String/Basic/LongestString/Main.java
@@ -0,0 +1,13 @@
+package String.Basic.LongestString;
+
+public class Main {
+    String longest(String[] names, int n) {
+        int max=0;
+        for(int i=0;i<n;i++){
+            if(names[i].length()>names[max].length()){
+                names[max]=names[i];
+            }
+        }
+        return names[max];
+    }
+}
