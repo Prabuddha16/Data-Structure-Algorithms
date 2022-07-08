76651f8ec68d21fa0bc1a1f4705d244457b8be90
Prabuddha16 <prabuddhakumar97@gmail.com>
2022-07-08 21:27:50 +0530

    update

---
+++ src/DP/Basic/NthTribonacciNumber/Main.java
@@ -0,0 +1,19 @@
+package DP.Basic.NthTribonacciNumber;
+
+public class Main {
+    public int tribonacci(int n) {
+        int[] dp = new int[n];
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2){
+            return 1;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i = 3; i < n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n-1] + dp[n-2] + dp[n-3];
+    }
+}
